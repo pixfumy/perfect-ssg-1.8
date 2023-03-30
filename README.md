e46a18815767e5e701a648fda4c0a205b7ae72e9
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-03-30 02:11:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Perfect SSG for 1.8
+
+## Features
+<!--todo-->
+
+## License
+This mod is available under the GPL3 license. Praise Stallman.
---
+++ src/main/java/me/pixfumy/perfect_ssg/PerfectSSG.java
@@ -0,0 +1,10 @@
+package me.pixfumy.perfect_ssg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PerfectSSG implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}

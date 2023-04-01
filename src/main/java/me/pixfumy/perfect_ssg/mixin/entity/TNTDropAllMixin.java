a6bd4ff1df74cd01576ac6c6bcd286e008434aae
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-04-01 01:39:19 -0400

    make tnt drop 50% instead of 25%

---
+++ src/main/java/me/pixfumy/perfect_ssg/mixin/entity/TNTDropAllMixin.java
@@ -0,0 +1,28 @@
+package me.pixfumy.perfect_ssg.mixin.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Explosion.class)
+public abstract class TNTDropAllMixin {
+    @Shadow @Final private Entity causingEntity;
+
+    /**
+     * Adapted from tildejustin/tnt-drop-all
+     */
+    @ModifyArg(method = "affectWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;randomDropAsItem(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;FI)V"))
+    private float changeTo1(float f) {
+        if (this.causingEntity instanceof TntEntity) {
+            return 0.5F;
+        }
+        return f;
+    }
+}

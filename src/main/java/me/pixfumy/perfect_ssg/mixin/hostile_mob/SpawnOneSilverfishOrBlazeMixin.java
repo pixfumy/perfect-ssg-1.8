9249a8479d1ce44623b0dfd26a54af3db4e29c7d
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-04-12 05:55:20 -0400

    limit blaze spawns to one per spawn cycle

--- src/main/java/me/pixfumy/perfect_ssg/mixin/hostile_mob/SpawnOneSilverfishOrBlazeMixin.java
+++ src/main/java/me/pixfumy/perfect_ssg/mixin/hostile_mob/SpawnOneSilverfishOrBlazeMixin.java
@@ -1,7 +1,6 @@
 package me.pixfumy.perfect_ssg.mixin.hostile_mob;
 
 import me.pixfumy.perfect_ssg.PerfectSSG;
-import me.pixfumy.perfect_ssg.mixin.access.SpawnerBlockEntityBehaviorAccess;
 import net.minecraft.block.entity.SpawnerBlockEntityBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,13 +8,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SpawnerBlockEntityBehavior.class)
-public abstract class SpawnOneSilverfishMixin {
-    @Shadow protected abstract String getEntityId();
+public abstract class SpawnOneSilverfishOrBlazeMixin {
+    @Shadow private String entityId;
 
     @Redirect(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/block/entity/SpawnerBlockEntityBehavior;spawnCount:I"))
     private int spawnCountTo1(SpawnerBlockEntityBehavior instance) {
-        if (((SpawnerBlockEntityBehaviorAccess)this).getEntityId().equals("Silverfish")) {
-            PerfectSSG.LOGGER.info("[Perfect SSG] Spawning singular silverfish.");
+        String entityId = this.entityId;
+        if (entityId.equals("Silverfish") || entityId.equals("Blaze")) {
+            PerfectSSG.LOGGER.info("[Perfect SSG] Spawning singular mob from spawner.");
             return 1;
         }
         return 4; // this shouldn't be hardcoded but who cares
